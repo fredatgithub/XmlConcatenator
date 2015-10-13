918912633cf314a904107506eace6e3b4d1ddf94
Freddy Juhel <domaine75@gmail.com>
2015-10-13 14:28:50 +0200

    Add controls

--- XmlConcatenator/FormMain.Designer.cs
+++ XmlConcatenator/FormMain.Designer.cs
@@ -71,6 +71,7 @@
       this.textBox2 = new System.Windows.Forms.TextBox();
       this.label2 = new System.Windows.Forms.Label();
       this.textBox3 = new System.Windows.Forms.TextBox();
+      this.button2 = new System.Windows.Forms.Button();
       this.menuStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
@@ -371,9 +372,9 @@
       // 
       this.button1.Location = new System.Drawing.Point(616, 68);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(40, 23);
       this.button1.TabIndex = 2;
-      this.button1.Text = "button1";
+      this.button1.Text = "...";
       this.button1.UseVisualStyleBackColor = true;
       // 
       // label1
@@ -419,11 +420,21 @@
       this.textBox3.Size = new System.Drawing.Size(866, 349);
       this.textBox3.TabIndex = 7;
       // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(616, 117);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(40, 23);
+      this.button2.TabIndex = 8;
+      this.button2.Text = "...";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
       // FormMain
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(911, 534);
+      this.Controls.Add(this.button2);
       this.Controls.Add(this.textBox3);
       this.Controls.Add(this.textBox2);
       this.Controls.Add(this.label2);
@@ -489,5 +500,6 @@
     private System.Windows.Forms.TextBox textBox2;
     private System.Windows.Forms.Label label2;
     private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Button button2;
   }
 }
