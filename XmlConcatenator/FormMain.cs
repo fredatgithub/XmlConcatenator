89a2a615d0ffddc54fdbe1890a4793ff1d38d411
fred <domaine75@gmail.com>
2015-12-20 00:35:47 +0100

    Update FormMain.cs

--- XmlConcatenator/FormMain.cs
+++ XmlConcatenator/FormMain.cs
@@ -531,7 +531,7 @@ namespace XmlConcatenator
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
